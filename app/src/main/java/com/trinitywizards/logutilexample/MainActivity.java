d65154d174202e033eef67e914744bddaa821998
ws.low <>
2018-10-16 10:46:30 +0800

    Added the upload j center code

--- app/src/main/java/com/trinitywizards/logutilexample/MainActivity.java
+++ app/src/main/java/com/trinitywizards/logutilexample/MainActivity.java
@@ -3,11 +3,15 @@ package com.trinitywizards.logutilexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.trinitywizards.logutil.LogDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogDebug.d("asd", "asd");
     }
 }
